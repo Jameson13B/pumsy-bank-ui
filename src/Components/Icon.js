0568560c99235c6d632899b5772c9ca22e4f67a2
Jameson13B <jamesonbrown@neolivity.com>
2019-05-14 17:27:36 -0600

    Created Icon component from Material Design

---
+++ src/Components/Icon.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const I = styled.i`
+  font-family: 'Material Icons';
+  font-size: 2em;
+  font-weight: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+  @font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2)
+      format('woff2');
+  }
+`
+
+const Icon = props => {
+  return <I className={props.className}>{props.icon}</I>
+}
+
+export default Icon
