0e9bad1ddca5a03929b889b8cf97c859276884a1
Jameson Brown <jamesonbrown@neolivity.com>
2019-06-12 18:31:35 -0600

    Created updateItem functionality

--- src/Components/StoreItemMgmt.js
+++ src/Components/StoreItemMgmt.js
@@ -1,75 +1,102 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import { database as db } from '../firebase'
-import ItemBtn from './StoreItemBtn'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { database as db } from "../firebase";
+import ItemBtn from "./StoreItemBtn";
 
 class ItemMgmt extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       items: [],
       feedback: null,
-      title: '',
-      description: '',
-      amount: '',
-      id: '',
+      title: "",
+      description: "",
+      amount: "",
+      id: "",
       creating: true
-    }
+    };
   }
   componentDidMount() {
     if (this.state.items.length === 0) {
-      db.collection('inventory')
+      db.collection("inventory")
         .get()
         .then(querySnapshot => {
-          let items = []
+          let items = [];
           querySnapshot.forEach(doc => {
-            const data = doc.data()
-            data.id = doc.id
-            items.push(data)
-          })
+            const data = doc.data();
+            data.id = doc.id;
+            items.push(data);
+          });
           // Sort buttons alphabetically
-          items.sort((a, b) => a.title > b.title ? 1 : -1)
-          this.setState({ items })
+          items.sort((a, b) => (a.title > b.title ? 1 : -1));
+          this.setState({ items });
         })
-        .catch(feedback => this.setState({feedback}))
+        .catch(feedback => this.setState({ feedback }));
     }
   }
-  handleInputChange = e => this.setState({ [e.target.name]: e.target.value })
-  handleUpdateSelect = (title, description, amount, id) => this.setState({title, description, amount, id, creating: false})
+  handleInputChange = e => this.setState({ [e.target.name]: e.target.value });
+  handleUpdateSelect = (title, description, amount, id) =>
+    this.setState({ title, description, amount, id, creating: false });
   deleteItem = (index, id) => {
-    const list = this.state.items.slice()
-    list.splice(index, 1)
-    db.collection('inventory')
+    const list = this.state.items.slice();
+    list.splice(index, 1);
+    db.collection("inventory")
       .doc(id)
       .delete()
       .then(() => {
-        this.setState({ items: list, title: '', description: '', amount: '', id: '', feedback: null })
+        this.setState({
+          items: list,
+          title: "",
+          description: "",
+          amount: "",
+          id: "",
+          feedback: null
+        });
       })
-      .catch(feedback => this.setState({feedback}))
-  }
-  createItem = (newItem) => {
-    const list = [...this.state.items, newItem].sort((a, b) => a.title > b.title ? 1 : -1)
-    db.collection('inventory')
+      .catch(feedback => this.setState({ feedback }));
+  };
+  createItem = newItem => {
+    const list = [...this.state.items, newItem].sort((a, b) =>
+      a.title > b.title ? 1 : -1
+    );
+    db.collection("inventory")
       .add(newItem)
       .then(res => {
-          this.setState({ items: list, title: '', description: '', amount: '', id: '', feedback: null  })
+        this.setState({
+          items: list,
+          title: "",
+          description: "",
+          amount: "",
+          id: "",
+          feedback: null
+        });
+      })
+      .catch(feedback => this.setState({ feedback }));
+  };
+  updateItem = newItem => {
+    const list = this.state.items.map(item => item.id === newItem.id ? newItem : item)
+    db.collection("inventory")
+      .doc(newItem.id)
+      .update(newItem)
+      .then(() =>
+        this.setState({
+          items: list,
+          title: "",
+          description: "",
+          amount: "",
+          id: "",
+          creating: true
         })
-        .catch(feedback => this.setState({feedback}))
-  }
-  updateItem = (newItem) => {
-    // Create Functionality
-      // const list = this.state.items.slice()
-      // list.splice(index, 1)
-      // db.collection('inventory')
-      //   .doc(newItem.id)
-      //   .update(newItem)
-      //   .then(() => {
-      //     this.setState({ items: list, title: '', description: '', amount: '', id: '', creating: true  })
-      //   })
-      //   .catch(feedback => this.setState({feedback}))
-  }
+      )
+      .catch(feedback => this.setState({ feedback }));
+  };
   render() {
-    const newItem = {title: this.state.title, description: this.state.description, amount: this.state.amount, id: this.state.id}
+    const newItem = {
+      title: this.state.title,
+      description: this.state.description,
+      amount: this.state.amount,
+      id: this.state.id
+    };
     return (
       <Container>
         <ItemList>
@@ -83,54 +110,59 @@ class ItemMgmt extends Component {
                 deleteItem={this.deleteItem}
                 handleClick={this.handleUpdateSelect}
               />
-            )
+            );
           })}
         </ItemList>
         {/* Add/Update Item Form */}
         <Form
-              onSubmit={e => {
-                e.preventDefault()
-                this.state.creating ? this.createItem(newItem) : this.updateItem(newItem)
-              }}
-              autoComplete='none'>
-              <Input
-                name='title'
-                type='text'
-                value={this.state.title}
-                placeholder='Item Title'
-                onChange={this.handleInputChange}
-              />
-              <Input
-                name='description'
-                type='text'
-                value={this.state.description}
-                placeholder='Item Description'
-                onChange={this.handleInputChange}
-              />
-              <Input
-                name='amount'
-                type='text'
-                value={this.state.amount}
-                placeholder='Item Amount'
-                onChange={this.handleInputChange}
-              />
-              <SubmitButton type='submit'>{this.state.creating ? 'Create Item' : 'Update Item'}</SubmitButton>
-              {this.state.feedback ? (
-                <Feedback>{this.state.feedback}</Feedback>
-              ) : null}
-            </Form>
+          onSubmit={e => {
+            e.preventDefault();
+            this.state.creating
+              ? this.createItem(newItem)
+              : this.updateItem(newItem);
+          }}
+          autoComplete="none"
+        >
+          <Input
+            name="title"
+            type="text"
+            value={this.state.title}
+            placeholder="Item Title"
+            onChange={this.handleInputChange}
+          />
+          <Input
+            name="description"
+            type="text"
+            value={this.state.description}
+            placeholder="Item Description"
+            onChange={this.handleInputChange}
+          />
+          <Input
+            name="amount"
+            type="text"
+            value={this.state.amount}
+            placeholder="Item Amount"
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton type="submit">
+            {this.state.creating ? "Create Item" : "Update Item"}
+          </SubmitButton>
+          {this.state.feedback ? (
+            <Feedback>{this.state.feedback}</Feedback>
+          ) : null}
+        </Form>
       </Container>
-    )
+    );
   }
 }
 
-export default ItemMgmt
+export default ItemMgmt;
 
 const Container = styled.div`
   padding: 20px;
   height: 89%;
   min-height: 0;
-`
+`;
 const ItemList = styled.div`
   display: flex;
   flex-direction: column;
@@ -138,14 +170,14 @@ const ItemList = styled.div`
   align-items: center;
   height: 60%;
   overflow: auto;
-`
+`;
 const Form = styled.form`
   flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 40%;
-`
+`;
 const Input = styled.input`
   background: transparent;
   border-top: 0;
@@ -158,7 +190,7 @@ const Input = styled.input`
   :focus {
     outline: none;
   }
-`
+`;
 const SubmitButton = styled.button`
   background: transparent;
   border: 1px solid white;
@@ -171,9 +203,9 @@ const SubmitButton = styled.button`
   :hover {
     background: #444;
   }
-`
+`;
 const Feedback = styled.p`
   color: red;
   font-size: 1rem;
   margin-top: 5px;
-`
+`;
